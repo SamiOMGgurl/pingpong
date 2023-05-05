807ec3544429006c538866d2df3228ebbefbf88a
SamiOMGgurl <83353299+SamiOMGgurl@users.noreply.github.com>
2023-05-05 16:00:28 +0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,48 @@
+noseX=0;
+noseY=0;
+cat="";
+
+function preload()
+{
+    cat = loadImage("Gatto!.png");
+}
+
+function setup() 
+{
+    canvas = createCanvas(400,240);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(400, 240);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+    }
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized');
+}
+
+function draw()
+{
+    image(video, 0, 0, 400, 240);
+    image(cat, noseX - 100, noseY - 148, 200, 200)
+}
+
+function t_s()
+{
+save('');
+}
